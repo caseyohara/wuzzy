1cba95711c04edc83dd9658e414318d12c2d267c
Casey O'Hara <casey.ohara@imulus.com>
2012-12-02 22:57:14 -0700

    Make main function a constructor for a wuzzy instance

--- test/wuzzy_test.js
+++ test/wuzzy_test.js
@@ -1,45 +1,46 @@
 require('should');
-var wuzzy = require('../lib/wuzzy').Wuzzy;
+var Wuzzy = require('../lib/wuzzy').Wuzzy;
+
+var phrases = [
+  "Optional multimedia contingency",
+  "Balanced radical help-desk",
+  "Polarised motivating throughput",
+  "Multi-channelled methodical projection",
+  "Virtual holistic intranet",
+  "Persistent dynamic data-warehouse",
+  "Customizable eco-centric intranet",
+  "Profound didactic ability",
+  "Adaptive maximized solution",
+  "Customer-focused mobile benchmark",
+  "Enhanced national interface",
+  "Multi-layered composite website",
+  "Devolved tangible monitoring",
+  "Reactive directional matrix",
+  "Secured object-oriented productivity",
+  "Triple-buffered mission-critical middleware",
+  "Versatile bifurcated toolset",
+  "Function-based solution-oriented toolset",
+  "Polarised interactive intranet",
+  "Multi-tiered client-server complexity"
+];
 
 describe("wuzzy", function() {
-
-  var phrases = [
-    "Optional multimedia contingency",
-    "Balanced radical help-desk",
-    "Polarised motivating throughput",
-    "Multi-channelled methodical projection",
-    "Virtual holistic intranet",
-    "Persistent dynamic data-warehouse",
-    "Customizable eco-centric intranet",
-    "Profound didactic ability",
-    "Adaptive maximized solution",
-    "Customer-focused mobile benchmark",
-    "Enhanced national interface",
-    "Multi-layered composite website",
-    "Devolved tangible monitoring",
-    "Reactive directional matrix",
-    "Secured object-oriented productivity",
-    "Triple-buffered mission-critical middleware",
-    "Versatile bifurcated toolset",
-    "Function-based solution-oriented toolset",
-    "Polarised interactive intranet",
-    "Multi-tiered client-server complexity"
-  ];
+  var wuzzy = new Wuzzy(phrases);
 
   it("finds exact matches", function() {
-    wuzzy('tangible', phrases).should.eql(["Devolved tangible monitoring"]);
+    wuzzy.search('tangible').should.eql(["Devolved tangible monitoring"]);
   });
 
   it("isn't case sensitive", function() {
-    wuzzy('MATRIX', phrases).should.eql(["Reactive directional matrix"]);
+    wuzzy.search('MATRIX').should.eql(["Reactive directional matrix"]);
   });
 
   it("isn't whitespace sensitive", function() {
-    wuzzy('bench mark', phrases).should.eql(["Customer-focused mobile benchmark"])
+    wuzzy.search('bench mark').should.eql(["Customer-focused mobile benchmark"])
   });
 
   it("sorts the results by length", function() {
-    wuzzy("multi", phrases).should.eql([
+    wuzzy.search("multi").should.eql([
       "Optional multimedia contingency",
       "Multi-layered composite website",
       "Multi-tiered client-server complexity",
@@ -48,11 +49,11 @@ describe("wuzzy", function() {
   });
 
   it("finds sequential matches", function() {
-    wuzzy("funbas", phrases).should.eql(["Function-based solution-oriented toolset"]);
+    wuzzy.search("funbas").should.eql(["Function-based solution-oriented toolset"]);
   });
 
   it("can drill down to an exact match", function() {
-    wuzzy("mti", phrases).should.eql([
+    wuzzy.search("mti").should.eql([
       "Adaptive maximized solution",
       "Reactive directional matrix",
       "Devolved tangible monitoring",
@@ -65,7 +66,7 @@ describe("wuzzy", function() {
       "Triple-buffered mission-critical middleware"
     ]);
 
-    wuzzy("mtic", phrases).should.eql([
+    wuzzy.search("mtic").should.eql([
       "Optional multimedia contingency",
       "Multi-layered composite website",
       "Customizable eco-centric intranet",
@@ -74,7 +75,7 @@ describe("wuzzy", function() {
       "Triple-buffered mission-critical middleware"
     ]);
 
-    wuzzy("mtict", phrases).should.eql([
+    wuzzy.search("mtict").should.eql([
       "Optional multimedia contingency",
       "Multi-layered composite website",
       "Customizable eco-centric intranet",
@@ -82,41 +83,43 @@ describe("wuzzy", function() {
       "Multi-channelled methodical projection"
     ]);
 
-    wuzzy("mtictn", phrases).should.eql([
+    wuzzy.search("mtictn").should.eql([
       "Optional multimedia contingency",
       "Customizable eco-centric intranet",
       "Multi-channelled methodical projection"
     ]);
 
-    wuzzy("mtictny", phrases).should.eql([
+    wuzzy.search("mtictny").should.eql([
       "Optional multimedia contingency"
     ]);
 
-    wuzzy("mtictnyz", phrases).should.eql([]);
+    wuzzy.search("mtictnyz").should.eql([]);
   });
 
-  describe("options", function() {
-    describe("html", function() {
-      it("wraps exact matches", function() {
-          var results = wuzzy("multi", phrases, {html: true});
+});
 
-          [ "Optional <b>multi</b>media contingency",
-            "<b>Multi</b>-layered composite website",
-            "<b>Multi</b>-tiered client-server complexity",
-            "<b>Multi</b>-channelled methodical projection"
-          ].forEach(function(result, index) {
-            results[index].should.eql(result);
-          });
-      });
+describe("options", function() {
+  describe("html", function() {
+    var wuzzy = new Wuzzy(phrases, {html: true});
 
-      it("wraps fuzzy matches", function() {
-        wuzzy("mtictny", phrases, {html: true}).should.eql([
-          "Optional <b>m</b>ul<b>ti</b>media <b>c</b>on<b>t</b>i<b>n</b>genc<b>y</b>"
-        ]);
-      });
+    it("wraps exact matches", function() {
+        var results = wuzzy.search("multi");
+
+        [ "Optional <b>multi</b>media contingency",
+          "<b>Multi</b>-layered composite website",
+          "<b>Multi</b>-tiered client-server complexity",
+          "<b>Multi</b>-channelled methodical projection"
+        ].forEach(function(result, index) {
+          results[index].should.eql(result);
+        });
+    });
+
+    it("wraps fuzzy matches", function() {
+      wuzzy.search("mtictny").should.eql([
+        "Optional <b>m</b>ul<b>ti</b>media <b>c</b>on<b>t</b>i<b>n</b>genc<b>y</b>"
+      ]);
     });
   });
 });
-
 
 
